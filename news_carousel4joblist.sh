0e14c4bae707655e7590504ea71db3b4ad5cbfe3
University of Ostrava Library <matyas.bajger@seznam.cz>
2022-01-03 09:59:12 +0100

    If insufficient or no new records found - seek back for more days
    Standard search is 8 days back.  If no or less than 8 recs found in this period, the seek is repeated but for 14 days back.  Than also 21 and 30 days back if needed.

--- news_carousel4joblist.sh
+++ news_carousel4joblist.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Script for scheduled runnning of news_carousel.csh
 # news_carousel.csh is problematic to execute by cron due to loading aleph env varibles by: $aleph_proc/def_local_env
@@ -8,4 +8,36 @@
 #define running news_carousel.csh including all arguments in this script and schedule running of this one in joblist
 #
 #Example for retrieving records from last week:
-#/path2script/news_carousel.csh -ccl "wdr=new`date '+%Y%m%d' --date='7 days ago'`->new`date '+%Y%m%d'`" -o /exlibris/aleph/u23_1/alephe/apache/htdocs/carousel
+#/news_carousel.csh -ccl "wdr=new`date '+%Y%m%d' --date='7 days ago'`->new`date '+%Y%m%d'`" -o /exlibris/aleph/u23_1/alephe/apache/htdocs/carousel
+
+#Sarka chctela zvhodit Bookport, po vetsim importu 20201009 tam zla sama beletrie
+#o vanocich 2020 byl vystupni soubor praydny, bo nebyly zadne novinky. Pridany kontroly vystupu a pokud je praydnz, lhuta & dnu se prodlouzi na !$ resp. @! dnu. 20210107 Matyas Bajger
+
+outputDir='/exlibris/aleph/u23_1/alephe/apache/htdocs/carousel'
+/exlibris/aleph/matyas/carousel/news_carousel.csh -ccl "wdr=`date '+%Y%m%d' --date='7 days ago'`->`date '+%Y%m%d'` not wps=Ve zpracovani not wbs=bookp*" -o $outputDir
+resultCount=0
+if [ -s $outputDir/carousel.xml]; then
+   resultCount=`xmllint --xpath 'count(//book)' $outputDir/carousel.xml`
+fi
+if [ $resultCount -lt 8 ]; then #try 14 days
+   echo WARNING - LESS THAN 8 RESULT FOUND, SEEKING 14 DAYS BACK
+   /exlibris/aleph/matyas/carousel/news_carousel.csh -ccl "wdr=`date '+%Y%m%d' --date='14 days ago'`->`date '+%Y%m%d'` not wps=Ve zpracovani not wbs=bookp*" -o $outputDir
+   if [ -s $outputDir/carousel.xml]; then
+      resultCount=`xmllint --xpath 'count(//book)' $outputDir/carousel.xml`
+   fi
+   if [  $resultCount -lt 8 ]; then #try 21 days
+      echo WARNING - LESS THAN 8 RESULT FOUND, SEEKING 21 DAYS BACK
+      /exlibris/aleph/matyas/carousel/news_carousel.csh -ccl "wdr=`date '+%Y%m%d' --date='21 days ago'`->`date '+%Y%m%d'` not wps=Ve zpracovani not wbs=bookp*" -o $outputDir
+      if [ -s $outputDir/carousel.xml]; then
+         resultCount=`xmllint --xpath 'count(//book)' $outputDir/carousel.xml`
+      fi
+      if [  $resultCount -lt 8 ]; then #try a month
+         echo WARNING - LESS THAN 8 RESULT FOUND, SEEKING A MONTH BACK
+         /exlibris/aleph/matyas/carousel/news_carousel.csh -ccl "wdr=`date '+%Y%m%d' --date='30 days ago'`->`date '+%Y%m%d'` not wps=Ve zpracovani not wbs=bookp*" -o $outputDir
+      fi
+   fi
+fi
+
+#/exlibris/aleph/matyas/carousel/news_carousel.csh -ccl "wdr=`date '+%Y%m%d' --date='20 days ago'`->`date '+%Y%m%d'` not wps=Ve zpracovani not wbs=bookp* not sbn=978-80-7642-664-1" -o /exlibris/aleph/u23_1/alephe/apache/htdocs/carousel
+~
+~
